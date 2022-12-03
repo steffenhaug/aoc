18f63489c712714007b7181cabcc395fb509b564
Steffen <stehau@pm.me>
2022-12-03 15:16:32 +0100

    Day 3 with lefward fold

--- 3/Main.hs
+++ 3/Main.hs
@@ -5,21 +5,24 @@ readlines f = do
   txt <- readFile f
   return (lines txt)
 
-group' n gs [] = gs
-group' n gs es =
+group'' n gs [] = gs
+group'' n gs es =
   let (g, e) = (splitAt n) es
-   in group' n (g : gs) e
+   in g : (group'' n gs e)
 
-group3 = group' 3 []
+group3 = group'' 3 []
 
 priority item
-  | item <= 'Z' = (ord item) - 38
-  | item <= 'z' = (ord item) - 96
-  | otherwise = 0
+  | item <> ('A', 'Z') = (ord item) - 38
+  | item <> ('a', 'z') = (ord item) - 96
+  | otherwise = undefined
+  where
+    x <> (a, b) = (a <= x) && (x <= b)
 
 main = do
-  lines <- readlines "input.txt"
+  lines <- readlines "test.txt"
   let groups = map (map Set.fromList) (group3 lines)
   let badges = map (foldl1 Set.intersection) groups
   let priorities = map (priority . Set.elemAt 0) badges
+  print badges
   print (sum priorities)
