c5073107566706f82015535a2bf34bf19dedd1c9
Steffen <stehau@pm.me>
2022-12-04 16:21:30 +0100

    Less whitespace

--- 4/Main.hs
+++ 4/Main.hs
@@ -1,16 +1,13 @@
 import Input
 
 x ∈ (a, b) = (a <= x) && (x <= b)
-
 (a, b) ⊆ i = (a ∈ i) && (b ∈ i)
-
 (a, b) ∩ (c, d) = a ∈ (c, d) || b ∈ (c, d) || c ∈ (a, b) || d ∈ (a, b)
 
-count f xs = length (filter f xs)
-
 contained (Assignments a b) = a ⊆ b || b ⊆ a
-
 overlapping (Assignments a b) = a ∩ b
+
+count f xs = length (filter f xs)
 
 one = do
   Right pairs <- input "input.txt"
