27af1264cb3cb6fc290fa7e0a28bec5d7f21f0f7
Steffen <stehau@pm.me>
2022-12-04 16:20:29 +0100

    I just gets worse

--- 4/Main.hs
+++ 4/Main.hs
@@ -4,13 +4,13 @@ x ∈ (a, b) = (a <= x) && (x <= b)
 
 (a, b) ⊆ i = (a ∈ i) && (b ∈ i)
 
-(a, b) ∩ i = (a ∈ i) || (b ∈ i)
+(a, b) ∩ (c, d) = a ∈ (c, d) || b ∈ (c, d) || c ∈ (a, b) || d ∈ (a, b)
 
 count f xs = length (filter f xs)
 
 contained (Assignments a b) = a ⊆ b || b ⊆ a
 
-overlapping (Assignments a b) = a ∩ b || b ∩ a
+overlapping (Assignments a b) = a ∩ b
 
 one = do
   Right pairs <- input "input.txt"
