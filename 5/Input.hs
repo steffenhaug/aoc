7ff54102ad837b4a78261edf423fa176c9bad82c
Steffen <stehau@pm.me>
2022-12-05 19:52:29 +0100

    renaming

--- 5/Input.hs
+++ 5/Input.hs
@@ -6,9 +6,12 @@ import qualified Data.IntMap as Map
 -- Read this weeks input data from a file and parse it.
 input f = do
   ls <- readlines f
+  -- Split input on the empty line.
   let (cs, ms) = split "" ls
-  let moves = map parse_move ms
+  -- Parse the crate stacks.
   let stacks = parse_stacks cs
+  -- Parse the crane moves.
+  let moves = map parse_move ms
   return (stacks, moves)
   where
     readlines = (fmap lines) . readFile
@@ -23,16 +26,14 @@ split at (x : xs)
       let (xs', tail) = split at xs
        in (x : xs', tail)
 
--- Parse a create movement "move n from x to y" into
--- a tuple (n, x, y)
+-- Parse a crate movement "move n from x to y" into a tuple (n, x, y)
 parse_move :: String -> (Int, Int, Int)
 parse_move move =
   let w = words move
       read' i = read (w !! i) :: Int
    in (read' 1, read' 3, read' 5)
 
--- Parse a layer of crates "[X] [Y]     [Z]" into
--- a map like { 1 => 'X', 2 => 'Y', 4 => 'Z' }
+-- Parse a layer of crates "[X] [Y]     [Z]" into a map.
 parse_crates str =
   let n = (length str + 1) `div` 4
       label i = str !! (4 * i - 3)
--- 5/Main.hs
+++ 5/Main.hs
@@ -3,48 +3,47 @@ import Data.IntMap (IntMap, elems, insert, (!))
 import qualified Data.IntMap as Map
 import Input
 
-type Move = (Int, Int, Int)
+type Stack = [Char]
 
-type Crane = Int -> ([Char], [Char]) -> ([Char], [Char])
+type Stacks = IntMap Stack
 
-type Stacks = IntMap [Char]
-
--- The CrateMover 9000
-crane :: Crane
-crane 0 (from, to) = (from, to)
-crane n (x : from, to) = crane (n - 1) (from, x : to)
-crane n ([], to) = undefined
+-- A crane move is a function that moves crates from one stack to another.
+type Move = Int -> (Stack, Stack) -> (Stack, Stack)
 
 -- Apply a CrateMover motion in the state monad.
-apply_move :: Crane -> Move -> (State Stacks) ()
-apply_move crane (n, from, to) = do
+apply_move :: Move -> (Int, Int, Int) -> (State Stacks) ()
+apply_move move (n, from, to) = do
   stacks <- get
   let f = stacks ! from
   let t = stacks ! to
-  let (f', t') = crane n (f, t)
+  let (f', t') = move n (f, t)
   let update = (insert from f') . (insert to t')
   put (update stacks)
   return ()
+
+-- The CrateMover 9000
+c9k 0 (from, to) = (from, to)
+c9k n (x : from, to) = c9k (n - 1) (from, x : to)
+c9k n ([], to) = undefined
 
 -- Find the labels on the top crates for a set of stacks.
 top_labels stacks = map head (elems stacks)
 
 one = do
   (stacks, moves) <- input "input.txt"
-  let procedure = traverse (apply_move crane) moves
+  let procedure = traverse (apply_move c9k) moves
   let result = execState procedure stacks
   print (top_labels result)
 
 -- The CrateMover 9001
-crane' :: Crane
-crane' 0 (from, to) = (from, to)
-crane' n (x : from, to) =
-  let (f, t) = crane' (n - 1) (from, to)
+c9k01 0 (from, to) = (from, to)
+c9k01 n (x : from, to) =
+  let (f, t) = c9k01 (n - 1) (from, to)
    in (f, x : t)
-crane' n ([], to) = undefined
+c9k01 n ([], to) = undefined
 
 two = do
   (stacks, moves) <- input "input.txt"
-  let procedure = traverse (apply_move crane') moves
+  let procedure = traverse (apply_move c9k01) moves
   let result = execState procedure stacks
   print (top_labels result)
