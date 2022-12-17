cf4457e5ee12d78958bf53c3af225f41471836cd
Steffen <stehau@pm.me>
2022-12-17 12:24:49 +0100

    Day 16 finally done

--- 16/Main.hs
+++ 16/Main.hs
@@ -1,19 +1,23 @@
-import Aoc (nat, printall)
-import Control.Monad.State
-import Data.Bits
+{-# LANGUAGE FlexibleContexts #-}
 import qualified Data.List as List
 import Data.Map (Map, (!))
 import qualified Data.Map as Map
 import Data.Set (Set)
 import qualified Data.Set as Set
-import Debug.Trace
+
+import Control.Monad.State
 import Text.Parsec hiding (State)
 
-{- Shotout to this guy:
+import Aoc (nat)
+
+{- Shoutout to this guy:
  -  https://github.com/juanplopes/advent-of-code-2022/blob/main/day16.py
- - For making me realize the critical information:
+ - reading his solution made me realize the critical information:
  -  After Floyd-Warshalling the graph, you ONLY ever visit a node once
- -  in a correct solution, which makes drastically reduces the search space.
+ -  in a correct solution, which drastically reduces the search space.
+ -
+ -  Even in part two, this works: The best way to play the game is the
+ -  maximum over pairwise optimal games with disjoint open valve sets.
  -}
 
 input file = do
@@ -31,7 +35,6 @@ input file = do
               [((i, j), 1) | i <- Map.keys v
                            , j <- Map.keys v
                            , connected i j ]
-
       -- Find the start node.
       Just s = List.findIndex ((=="AA") . name) vl
       start = 1 + s
@@ -46,7 +49,6 @@ input file = do
                  ]
           edges <-  (sepBy (many upper) (string ", "))
           pure (Valve name flow, edges)
-
 
 data Valve
   = Valve
@@ -66,55 +68,62 @@ data Graph
       }
   deriving (Show)
 
+floyd_warshall g = g { adj = adj' }
+  where adj' = execState (traverse fw updates) initial_edgeset
+        -- Create the initial edge set.
+        initial_edgeset =
+          Map.fromList [((i, j), edge i j) | i <- [1..nvs g]
+                                           , j <- [1..nvs g]]
+        edge i j
+          | (i, j) `Map.member` adj g = 1
+          | i == j = 0
+          | otherwise = 666
 
-floyd_warshall g =
-  let initial = Map.fromList
-        [((i, j), if (i, j) `Map.member` adj g then 1
-                  else if i == j then 0
-                  else 666)
-          | i <- [1..nvs g]
-          , j <- [1..nvs g]]
-   in g { adj=execState fw initial }
-  where fw = do
-          let update :: (Int, Int, Int) -> State Adj ()
-              update (i, j, k) = do
-                e_ij <- gets (! (i, j))
-                e_ik <- gets (! (i, k))
-                e_kj <- gets (! (k, j))
-                modify $ Map.insert (i, j) $ min e_ij (e_ik+e_kj)
-          traverse update [(i, j, k) | k <- [1..nvs g]
-                                     , j <- [1..nvs g]
-                                     , i <- [1..nvs g]]
+        -- Traverse these locations, and update the shortest path.
+        updates = [(i, j, k) | k <- [1..nvs g]
+                             , j <- [1..nvs g]
+                             , i <- [1..nvs g]]
+        fw (i, j, k) = do
+          e_ij <- gets (! (i, j))
+          e_ik <- gets (! (i, k))
+          e_kj <- gets (! (k, j))
+          modify $ Map.insert (i, j) (min e_ij (e_ik+e_kj))
 
-
-search :: Graph -> Int -> Map (Set Int) Int
 search graph t = execState (search' graph t Set.empty 0 (start graph)) Map.empty
-  where
-    search' g t open f v =
-      -- If we are out of time, don't do anything more.
-      if t <= 0 then pure ()
-      else do
-        -- Keep track of the best possible flow for a given set of open vents.
-        max_f <- gets $ (Map.findWithDefault 0) open
-        modify $ Map.insert open (max max_f f)
+  where search' g t open f v
+          | t <= 0 = pure ()
+          | otherwise = do
+              -- Keep track of the best possible flow for a given set of open vents.
+              let cache Nothing      = Just f
+                  cache (Just f_old) = Just (max f f_old)
+              modify $ Map.alter cache open
 
-        -- Visit remaining candidates.
-        let candidates = Map.filter ((/= 0) . flow) (valves g)
-            bithacking w _ = w `Set.notMember` open
-            unvisited = Map.filterWithKey bithacking candidates
+              -- Visit remaining candidates.
+              let candidates = Map.filter ((/= 0) . flow) (valves g)
+                  bithacking w _ = w `Set.notMember` open
+                  unvisited = Map.filterWithKey bithacking candidates
 
-            visit :: Int -> State (Map (Set Int) Int) ()
-            visit u =
-              let t' = t - (adj g ! (v, u)) - 1
-                  f' = f + t' * (flow (valves g ! u))
-               in search' g t' (Set.insert u open) f' u
+                  visit :: Int -> State (Map (Set Int) Int) ()
+                  visit u =
+                    let t' = t - (adj g ! (v, u)) - 1
+                        f' = f + t' * (flow (valves g ! u))
+                     in search' g t' (Set.insert u open) f' u
 
-        mapM_ visit (Map.keys unvisited)
+              mapM_ visit (Map.keys unvisited)
 
 main = do
   graph <- input "input.txt"
 
   let graph'  = floyd_warshall graph
-      visited = search graph' 30
 
+  -- Part one.
+  let visited = search graph' 30
   print $ maximum (Map.elems visited)
+
+  -- Part two.
+  let visited = search graph' 26
+      release = [v1 + v2 | (s1, v1) <- Map.toList visited
+                         , (s2, v2) <- Map.toList visited
+                         , Set.disjoint s1 s2]
+
+  print $ maximum release
