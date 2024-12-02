5e9b432f5d38c091fd95c3bac9e870561e431c7d
Steffen Haug <st@haug.codes>
2024-12-02 21:48:16 +0100

    include test file in repo since its public anyways

--- 2024/2/main.jl
+++ 2024/2/main.jl
@@ -1,10 +1,10 @@
 using Combinatorics
 
-reports = [parse.(Int64, line) for line in split.(readlines(open("input.txt")))]
+reports = [parse.(Int64, line) for line in split.(readlines(open("test.txt")))]
 
 function is_safe(report)
   delta = diff(report)
-  all(1 .<= delta .<= 3) || all(-1 .>= delta .>= -3)
+  all(@. 1 <= delta <= 3) || all(@. -1 >= delta >= -3)
 end
 
 # 0.000129 seconds (7.19 k allocations: 263.797 KiB)
@@ -12,7 +12,8 @@ p1(reports) = sum(is_safe.(reports))
 
 function is_safe_with_dampener(report)
   N = length(report)
-  any(is_safe.(combinations(report, N - 1)))
+  removed = combinations(report, N - 1)
+  any(is_safe.(removed))
 end
 
 # 0.001488 seconds (75.71 k allocations: 2.863 MiB)
