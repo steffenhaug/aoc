0910edc1424b6deb6b532ef014c85c0b4b13fb50
Steffen Haug <st@haug.codes>
2024-12-03 08:06:06 +0100

    cleaning up d2

--- 2024/2/main.jl
+++ 2024/2/main.jl
@@ -1,20 +1,22 @@
 using Combinatorics
 
-reports = [parse.(Int64, line) for line in split.(readlines(open("test.txt")))]
-
 function is_safe(report)
   delta = diff(report)
-  all(@. 1 <= delta <= 3) || all(@. -1 >= delta >= -3)
+  all(@. 1 ≤ delta ≤ 3) || all(@. -1 ≥ delta ≥ -3)
 end
-
-# 0.000129 seconds (7.19 k allocations: 263.797 KiB)
-p1(reports) = sum(is_safe.(reports))
 
 function is_safe_with_dampener(report)
-  N = length(report)
-  removed = combinations(report, N - 1)
-  any(is_safe.(removed))
+  any(is_safe.(combinations(report, length(report) - 1)))
 end
 
-# 0.001488 seconds (75.71 k allocations: 2.863 MiB)
-p2(reports) = sum(is_safe_with_dampener.(reports))
+function solve(file)
+  reports = [parse.(Int64, line) for line in split.(readlines(open(file)))]
+
+  p1 = sum(is_safe.(reports))
+  p2 = sum(is_safe_with_dampener.(reports))
+
+  println(p1)
+  println(p2)
+end
+
+solve("test.txt")
